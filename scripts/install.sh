37754f0b51271b9807e14f895ed83436a9463396
RNO <arnaud.juracek@gmail.com>
2020-03-24 14:11:21 +0100

    Fix install.sh path

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+DIR="$(cd `dirname $0` && pwd)"
 
 curl -L https://github.com/chevalvert/www-paysagescotesdemoselle-tiles/releases/download/bin/package.zip > package.zip && \
 unzip package.zip -d $DIR/../data && \
