fdd64b2fb27a1e9a788d00d59ebe8bd279308d4a
RNO <arnaud.juracek@gmail.com>
2020-03-24 13:57:59 +0100

    Update ppmm-builder to 3.7.x
    See chevalvert/ppmm-builder for more details

--- l-hydrographie/render.sh
+++ l-hydrographie/render.sh
@@ -3,43 +3,11 @@ DATA="$DIR/../data"
 STYLE="$DIR/style.json"
 BBOX='917147.156155741,6885201.29637512,937157.321376621,6903953.67978212'
 
-rm -rf $DIR/{1,2,3,4,5}
+rm -rf $DIR/{1..7}
 cat $DATA/{C04,C03}.geojson | ppmm-builder \
   --progress \
   --output $DIR \
   --bbox $BBOX \
   --styles $STYLE \
   --resolution 144 \
-  --skip-empty \
-  --zoom '1,2,3,4,5'
-
-rm -rf $DIR/6
-cat $DATA/{C04,C03}.geojson | ppmm-builder \
-  --progress \
-  --output $DIR \
-  --bbox $BBOX \
-  --styles $STYLE \
-  --skip-empty \
-  --zoom 6 \
-
-rm -rf $DIR/7
-let zoom=7
-let max=$[2 ** $zoom]
-let steps=$[2 ** ($zoom - 6)]
-let tilesize=$[$max / $steps]
-for i in $(seq 0 $[$steps - 1]); do
-  for j in $(seq 0 $[$steps - 1]); do
-    let x1=$[$tilesize * $i]
-    let y1=$[$tilesize * $j]
-    let x2=$[$tilesize * ($i + 1)]
-    let y2=$[$tilesize * ($j + 1)]
-    cat $DATA/{C04,C03}.geojson | ppmm-builder \
-      --progress \
-      --output $DIR \
-      --bbox $BBOX \
-      --styles $STYLE \
-      --skip-empty \
-      --zoom $zoom \
-      --region "$x1,$y1,$x2,$y2"
-  done;
-done;
+  --zoom '1,2,3,4,5,6,7'
--- le-socle/render.sh
+++ le-socle/render.sh
@@ -4,6 +4,7 @@ STYLE="$DIR/style.json"
 RASTERS="$DIR/rasters.json"
 BBOX='917147.156155741,6885201.29637512,937157.321376621,6903953.67978212'
 
+rm -rf $DIR/{1..7}
 cat $DATA/{C05-10m,C05}.geojson | ppmm-builder \
   --progress \
   --output $DIR \
@@ -11,33 +12,4 @@ cat $DATA/{C05-10m,C05}.geojson | ppmm-builder \
   --styles $STYLE \
   --rasters $RASTERS \
   --resolution 144 \
-  --zoom '1,2,3,4,5'
-
-cat $DATA/{C05-10m,C05}.geojson | ppmm-builder \
-  --progress \
-  --output $DIR \
-  --bbox $BBOX \
-  --styles $STYLE \
-  --rasters $RASTERS \
-  --zoom 6
-
-let zoom=7
-let max=$[2 ** $zoom]
-let steps=$[2 ** ($zoom - 6)]
-let tilesize=$[$max / $steps]
-for i in $(seq 0 $[$steps - 1]); do
-  for j in $(seq 0 $[$steps - 1]); do
-    let x1=$[$tilesize * $i]
-    let y1=$[$tilesize * $j]
-    let x2=$[$tilesize * ($i + 1)]
-    let y2=$[$tilesize * ($j + 1)]
-    cat $DATA/{C05-10m,C05}.geojson | ppmm-builder \
-      --progress \
-      --output $DIR \
-      --bbox $BBOX \
-      --styles $STYLE \
-      --rasters $RASTERS \
-      --zoom $zoom \
-      --region "$x1,$y1,$x2,$y2"
-  done;
-done;
+  --zoom '1,2,3,4,5,6,7'
