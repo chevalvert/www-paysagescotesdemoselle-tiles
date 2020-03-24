11eeff63a18ccd5fee97db8986b1d0fe968d9a13
RNO <arnaud.juracek@gmail.com>
2020-03-24 14:54:00 +0100

    [les-espaces-agricoles] Initial commit

---
+++ les-espaces-agricoles/render.sh
@@ -0,0 +1,13 @@
+DIR="$(cd `dirname $0` && pwd)"
+DATA="$DIR/../data"
+STYLE="$DIR/style.json"
+BBOX='917147.156155741,6885201.29637512,937157.321376621,6903953.67978212'
+
+rm -rf $DIR/{1..7}
+cat $DATA/{C23,C24,C25}.geojson | ppmm-builder \
+  --progress \
+  --output $DIR \
+  --bbox $BBOX \
+  --styles $STYLE \
+  --resolution 144 \
+  --zoom '1,2,3,4,5,6,7'
