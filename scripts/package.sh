05b7b41dfdbe184410b51ba4c79cf2b76e328744
RNO <arnaud.juracek@gmail.com>
2020-03-27 16:04:07 +0100

    Improve package.sh

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,4 +1,5 @@
 DIR="$(cd `dirname $0` && pwd)"
 zip -FSrj package.zip $DIR/../data
-echo 'Packaging done. Go to'
-echo '→ https://github.com/chevalvert/www-paysagescotesdemoselle-tiles/releases/edit/bin'
+echo 'Packaging done.'
+open 'https://github.com/chevalvert/www-paysagescotesdemoselle-tiles/releases/edit/bin'
+open .
