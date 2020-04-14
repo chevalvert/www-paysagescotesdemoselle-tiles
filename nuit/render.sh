be357f9818a44e3263fbc8f8f7d3c5a71130dc29
RNO <arnaud.juracek@gmail.com>
2020-04-14 17:14:12 +0200

    [nuit] Initial commit

---
+++ nuit/render.sh
@@ -0,0 +1,15 @@
+DIR="$(cd `dirname $0` && pwd)"
+DATA="$DIR/../data"
+STYLE="$DIR/style.json"
+RASTERS="$DIR/rasters.json"
+BBOX='917147.156155741,6885201.29637512,937157.321376621,6903953.67978212'
+
+rm -rf $DIR/{1..7}
+cat $DATA/{C05-10m,C05,C21,C04,C03,C08,C09,C10,C16,C17}.geojson | ppmm-builder \
+  --progress \
+  --output $DIR \
+  --bbox $BBOX \
+  --styles $STYLE \
+  --rasters $RASTERS \
+  --resolution 144 \
+  --zoom '1,2,3,4,5,6,7'
