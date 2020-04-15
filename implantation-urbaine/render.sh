36b3748d760608b1fdd9f41f214a691953417a21
RNO <arnaud.juracek@gmail.com>
2020-04-15 11:28:57 +0200

    [implantation-urbaine] Fix render.sh using debug datas

--- implantation-urbaine/render.sh
+++ implantation-urbaine/render.sh
@@ -4,8 +4,7 @@ STYLE="$DIR/style.json"
 BBOX='917147.156155741,6885201.29637512,937157.321376621,6903953.67978212'
 
 rm -rf $DIR/{1..7}
-# cat $DATA/{C49,C08,C09,C34,C35,C32}.geojson | ppmm-builder \
-cat $DATA/{C49,C09,C34,C35,C32-debug}.geojson | ppmm-builder \
+cat $DATA/{C49,C08,C09,C34,C35,C32}.geojson | ppmm-builder \
   --progress \
   --output $DIR \
   --bbox $BBOX \
